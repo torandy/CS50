139a5dd5bcf0ea385836d3564f3bc114cf8ae6cd
Randy To <77013214+torandy@users.noreply.github.com>
2022-02-19 21:04:54 -0500

    Update scrabble.c

--- Week 2/scrabble/scrabble.c
+++ Week 2/scrabble/scrabble.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+// Determine which of two Scrabble words is worth more based on the points value defined.
+
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
